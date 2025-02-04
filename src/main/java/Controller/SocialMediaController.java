d846f3cead5312367845aaf865bf3c559c86a275
Alex Papka <Alexpapka@yahoo.com>
2025-02-04 21:44:17 +0000

    SocialMediaController built except for delete

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,12 @@
 package Controller;
 
+import org.eclipse.jetty.http.HttpTester.Message;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -7,8 +14,15 @@ import io.javalin.http.Context;
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
+ */ 
 public class SocialMediaController {
+    AccountService accountService;  // class variable
+    MessageService messageService;  // class variable
+
+    public SocialMediaController(){     // constructor
+        this.accountService = new AccountService();
+        this.messageService = new MessageService(); 
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,8 +30,16 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("register", this::registerHandler);
+        app.post("login", this::loginHandler);
+        app.post("messages", this::postMessageHandler);
+        app.get("messages", this::getAllMessagesHandler);
+        app.get("messages/{message_id}", this::getMessageById);
+        app.delete("messages/{message_id}", this::deleteMessageById);
+        app.patch("messages/{message_id}", this::patchMessageById);
+        app.get("accounts/{account_id}/messages", this::getAllAccountMessageHandler);
 
+        app.start(8080);
         return app;
     }
 
@@ -29,5 +51,64 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+    private void registerHandler(Context ctx){
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = AccountService.addAccount(account);              // account service not made yet
+        if(addedAccount != null){
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        }else{
+            ctx.status(400);
+        }
+    }
+
+    private void loginHandler(Context ctx){
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account getAccount = AccountService.getAccount(account);            // account service not made yet
+        if(getAccount != null){
+            ctx.json(mapper.writeValueAsString(getAccount));
+        }else{
+            ctx.status(401);
+        }
+    }
+
+    private void postMessageHandler(Context ctx){
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        Message messageText = MessageService.postMessage(message);      // message service not made yet
+        if(messageText != null){
+            ctx.json(mapper.writeValueAsString(messageText));
+        }else{
+            ctx.status(400);
+        }
+    }
+
+    private void getAllMessagesHandler(Context ctx){
+        ctx.json(messageService.getAllMessages());      // messageService doesnt exist yet
+    }
+
+    private void getMessageById(Context ctx){
+        ctx.json(messageService.getMessageById(ctx.pathParam("message_id")));   // messageService doesnt exist yet
+    }
+
+    private void deleteMessageById(Context ctx){
+        // dont know yet
+    }
+
+    private void patchMessageById(Context ctx){
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        Message messageChanged = MessageService.patchMessage(ctx.pathParam("message_id"));    // Message service Doesnt exist yet
+        if(messageChanged != null){
+            ctx.json(mapper.writeValueAsString(messageChanged));
+        }else{
+            ctx.status(400);
+        }
+    }
+
+    private void getAllAccountMessageHandler(Context ctx){
+        ctx.json(messageService.getMessageByAccount(ctx.pathParam("account_id")));  // MessageService doesnt exist yet
+    }
 
 }
---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,7 @@
+package Service;
+
+import java.util.List;
+
+public class MessageService {
+    
+}
